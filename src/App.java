415bcd825baa4c0340c77bd54947f2dbf80844cb
LeAnne B. <branchleannelmb@gmail.com>
2025-02-03 18:05:00 -0500

    makes game window

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+
+import javax.swing.JFrame;
+
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+
+        JFrame frame = new JFrame("Pac Man");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
